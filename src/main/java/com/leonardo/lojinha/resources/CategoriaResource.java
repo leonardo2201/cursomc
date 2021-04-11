f41c05f1f48850351741cb87409b4f8d11c5d59f
Leonardo <leo.cristiano.oliveira@gmail.com>
2021-04-11 17:32:59 -0300

    Banco h2

--- src/main/java/com/leonardo/lojinha/resources/CategoriaResource.java
+++ src/main/java/com/leonardo/lojinha/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.leonardo.lojinha.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.leonardo.lojinha.entity.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "REST está funcionando";
+	public List<Categoria> Listar() {
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat2);
+		list.add(cat1);
+		
+		return list;
 	}
 }
